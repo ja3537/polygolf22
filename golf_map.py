3291ebe883d6bfd0e760eecd8e882baef8ec1ccb
Joe Adams <78444947+ja3537@users.noreply.github.com>
2022-09-10 14:36:42 -0400

    refactored to allow for maps without sand traps

--- golf_map.py
+++ golf_map.py
@@ -16,20 +16,23 @@ class GolfMap:
         self.start = sympy.geometry.Point2D(*json_obj["start"])
         self.target = sympy.geometry.Point2D(*json_obj["target"])
         self.golf_map = sympy.Polygon(*json_obj["map"])
-        sand_traps = list(json_obj["sand traps"])
         self.sand_traps = []
-        for s in sand_traps:
-            trap = sympy.Polygon(*s)
-            self.sand_traps.append(trap)
-        for s1 in self.sand_traps:
-            assert self.golf_map.encloses(s1), "Golf map must enclose all sand traps"
-            assert not s1.encloses(self.start), "Sand traps may not contain start"
-            assert not s1.encloses(self.target), "Sand traps may not contain target"
-            for s2 in self.sand_traps:
-                if s1 == s2:
-                    continue
-                intersection = s1.intersection(s2)
-                assert not intersection, "sand traps may not intersect"
+
+        if 'sand traps' in json_obj:
+            sand_traps = list(json_obj["sand traps"])
+            for s in sand_traps:
+                trap = sympy.Polygon(*s)
+                self.sand_traps.append(trap)
+
+            for s1 in self.sand_traps:
+                assert self.golf_map.encloses(s1), "Golf map must enclose all sand traps"
+                assert not s1.encloses(self.start), "Sand traps may not contain start"
+                assert not s1.encloses(self.target), "Sand traps may not contain target"
+                for s2 in self.sand_traps:
+                    if s1 == s2:
+                        continue
+                    intersection = s1.intersection(s2)
+                    assert not intersection, "sand traps may not intersect"
 
         assert self.golf_map.encloses(self.start), "Start point doesn't lie inside map polygon"
         assert self.golf_map.encloses(self.target), "Target point doesn't lie inside map polygon"
