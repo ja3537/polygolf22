e9253d6a894d684f535b8e29f67038c4fa049654
Jeff Tao <jatao88@gmail.com>
2022-09-16 15:31:04 -0400

    Render to svg, add shot count bar chart

--- plot_trials.py
+++ plot_trials.py
@@ -1,47 +1,76 @@
 import matplotlib.pyplot as plt
 import json
+import jsonlines
 import sys
 
 if len(sys.argv) != 2:
     print("Usage: python plot_trials.py <log_file.json>")
     exit()
 
-with open(sys.argv[1]) as f:
-    log = json.loads(f.read())
+with jsonlines.open(sys.argv[1]) as reader:
+    logs = list(reader)
 
-with open(log["map"]) as f:
+with open(logs[0]["map"]) as f:
     map = json.loads(f.read())
 
 plt.figure(facecolor="#bbddff")
-plt.axis("off")
-ax = plt.gca()
-ax.invert_yaxis()
 
-plt.fill(*list(zip(*map["map"])), facecolor="#bbff66", edgecolor="black", linewidth=1)
+xs = [x for x, _ in map["map"]]
+ys = [y for y, _ in map["map"]]
+width = max(xs) - min(xs)
+height = max(ys) - min(ys)
+
+fig, (ax1, ax2) = plt.subplots(2, 1, gridspec_kw={"height_ratios": [2, 1]})
+
+ax1.set_aspect(width / height)
+ax1.axis("off")
+ax1.invert_yaxis()
+
+ax1.fill(*list(zip(*map["map"])), facecolor="#bbff66", edgecolor="black", linewidth=1)
 
 start_x, start_y = map["start"]
-plt.plot(start_x, start_y, "bo")
+ax1.plot(start_x, start_y, "bo")
 target_x, target_y = map["target"]
-plt.plot(target_x, target_y, "ro")
+ax1.plot(target_x, target_y, "ro")
 
 if "sand traps" in map:
     for trap in map["sand traps"]:
-        plt.fill(*list(zip(*trap)), facecolor="#ffffcc", edgecolor="black", linewidth=1)
+        ax1.fill(*list(zip(*trap)), facecolor="#ffffcc", edgecolor="black", linewidth=1)
 
 
-lands = log["landing_history"]["0"]
-ends = log["ending_history"]["0"]
+opacity = max(1 / len(logs), 0.05)
+for log in logs:
+    lands = log["landing_history"]["0"]
+    ends = log["ending_history"]["0"]
 
-last_x = start_x
-last_y = start_y
-for n in range(len(ends)):
-    land_x, land_y = lands[n]
-    end_x, end_y = ends[n]
-    plt.plot([last_x, land_x], [last_y, land_y], color="black", alpha=0.2)
-    # plt.plot([land_x, end_x], [land_y, end_y], color="black", alpha=0.2)
-    plt.arrow(land_x, land_y, end_x - land_x, end_y - land_y, color="black", alpha=0.5)
-    last_x = end_x
-    last_y = end_y
+    last_x = start_x
+    last_y = start_y
+    for n in range(len(ends)):
+        land_x, land_y = lands[n]
+        end_x, end_y = ends[n]
+        ax1.plot([last_x, land_x], [last_y, land_y], color="black", alpha=opacity)
+        ax1.arrow(
+            land_x,
+            land_y,
+            end_x - land_x,
+            end_y - land_y,
+            color="black",
+            alpha=0.5,
+            head_width=4,
+            head_length=4,
+            length_includes_head=True,
+        )
+        last_x = end_x
+        last_y = end_y
 
 
-plt.savefig("render.png", dpi=200)
+shot_count = {}
+for log in logs:
+    score = log["scores"][0]
+    if not score in shot_count:
+        shot_count[score] = 0
+    shot_count[score] += 1
+
+ax2.bar(*list(zip(*shot_count.items())))
+
+plt.savefig("render.svg")
