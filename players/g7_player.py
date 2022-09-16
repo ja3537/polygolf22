3e54a4b89da785b621f5b02fec96ac94e6c03df7
Martin Ristovski <martin.ristovski@columbia.edu>
2022-09-16 18:18:26 -0400

    add polygon_to_np_points; complete grid building

--- players/g7_player.py
+++ players/g7_player.py
@@ -1,11 +1,13 @@
 import numpy as np
+import scipy
 import sympy
 import logging
 from typing import Tuple, Iterator
+from shapely.geometry import Polygon as ShapelyPolygon
 
 STEP = 1.0 # chunk size == 1m
 
-def make_grid(polygon_map: sympy.Polygon) -> Iterator[Tuple[float, float]]:
+def polygon_to_points(golf_map: sympy.Polygon) -> Iterator[Tuple[float, float]]:
     """
     This function takes in the polygon golf map and returns an iterator for the
     points on a lattice with distance STEP. We ignore the edges of the map
@@ -13,7 +15,7 @@ def make_grid(polygon_map: sympy.Polygon) -> Iterator[Tuple[float, float]]:
     """
     x_min, x_max, y_min, y_max = float('inf'), float('-inf'), float('inf'), float('-inf')
 
-    for vertex in polygon_map.vertices:
+    for vertex in golf_map.vertices:
         x, y = float(vertex.x), float(vertex.y)
 
         x_min = min(x_min, x)
@@ -31,6 +33,15 @@ def make_grid(polygon_map: sympy.Polygon) -> Iterator[Tuple[float, float]]:
         x_curr += step
 
 
+def sympy_polygon_to_shapely(polygon: sympy.Polygon) -> ShapelyPolygon:
+    """
+    Helper function that converts a sympy polygon to a shapely polygon
+    """
+    vertices = polygon.vertices
+    vertices.append(vertices[0])
+    return ShapelyPolygon(vertices)
+
+
 class Player:
     def __init__(self, skill: int, rng: np.random.Generator, logger: logging.Logger, golf_map: sympy.Polygon, start: sympy.geometry.Point2D, target: sympy.geometry.Point2D, sand_traps: list[sympy.geometry.Point2D], map_path: str, precomp_dir: str) -> None:
         """Initialise the player with given skill.
@@ -46,11 +57,32 @@ class Player:
             precomp_dir (str): Directory path to store/load precomputation
         """
 
-        self.conf = 0.95 # confidence level
+        self.conf = 0.95 # confidence level, we might want to decrease this for low skill players
         self.skill = skill
         self.rng = rng
         self.logger = logger
-        self.np_grid = None
+        self.target = None
+        self.np_points = None
+        self.shapely_polygon = None
+
+
+    def polygon_to_np_points(self, target: Tuple[float, float], golf_map: sympy.Polygon):
+        """
+        Gets the points within the polygon and stores them in a numpy array 
+        along with their distances to the target.
+        """
+        points = [target]
+        self.shapely_polygon = sympy_polygon_to_shapely(golf_map)
+
+        polygon_points = polygon_to_points(golf_map)
+        for point in polygon_points:
+            if self.shapely_polygon.contains(point):
+                x, y = point
+                points.append(np.array([x, y]))
+        
+        self.np_points = np.array(points)
+        self.np_dist_to_target = scipy.spatial.distance.cdist(self.np_points, np.array([np.array(self.target)]), 'euclidean')
+        self.np_dist_to_target = self.np_dist_to_target.flatten()
 
 
     def play(self, score: int, golf_map: sympy.Polygon, target: sympy.geometry.Point2D, sand_traps: list[sympy.geometry.Point2D], curr_loc: sympy.geometry.Point2D, prev_loc: sympy.geometry.Point2D, prev_landing_point: sympy.geometry.Point2D, prev_admissible: bool) -> Tuple[float, float]:
