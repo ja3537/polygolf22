f7e6fda847e297463767a257f61781eb59cb07a2
Jeff Tao <jatao88@gmail.com>
2022-09-17 14:42:27 -0400

    Combine multiple trials from multiple skill levels in one render

--- plot_trials.py
+++ plot_trials.py
@@ -1,76 +1,110 @@
 import matplotlib.pyplot as plt
 import json
-import jsonlines
 import sys
+import os.path as path
+import os
 
-if len(sys.argv) != 2:
-    print("Usage: python plot_trials.py <log_file.json>")
-    exit()
-
-with jsonlines.open(sys.argv[1]) as reader:
-    logs = list(reader)
-
-with open(logs[0]["map"]) as f:
-    map = json.loads(f.read())
-
-plt.figure(facecolor="#bbddff")
-
-xs = [x for x, _ in map["map"]]
-ys = [y for y, _ in map["map"]]
-width = max(xs) - min(xs)
-height = max(ys) - min(ys)
-
-fig, (ax1, ax2) = plt.subplots(2, 1, gridspec_kw={"height_ratios": [2, 1]})
-
-ax1.set_aspect(width / height)
-ax1.axis("off")
-ax1.invert_yaxis()
-
-ax1.fill(*list(zip(*map["map"])), facecolor="#bbff66", edgecolor="black", linewidth=1)
-
-start_x, start_y = map["start"]
-ax1.plot(start_x, start_y, "bo")
-target_x, target_y = map["target"]
-ax1.plot(target_x, target_y, "ro")
-
-if "sand traps" in map:
-    for trap in map["sand traps"]:
-        ax1.fill(*list(zip(*trap)), facecolor="#ffffcc", edgecolor="black", linewidth=1)
+PLOT_DIR = "plots"
 
 
-opacity = max(1 / len(logs), 0.05)
-for log in logs:
-    lands = log["landing_history"]["0"]
-    ends = log["ending_history"]["0"]
+def plot_trials(logs):
+    num_skill_levels = len(logs)
+    map_path = logs[0]["logs"][0]["map"]
 
-    last_x = start_x
-    last_y = start_y
-    for n in range(len(ends)):
-        land_x, land_y = lands[n]
-        end_x, end_y = ends[n]
-        ax1.plot([last_x, land_x], [last_y, land_y], color="black", alpha=opacity)
-        ax1.arrow(
-            land_x,
-            land_y,
-            end_x - land_x,
-            end_y - land_y,
-            color="black",
-            alpha=0.5,
-            head_width=4,
-            head_length=4,
-            length_includes_head=True,
+    with open(map_path) as f:
+        map = json.loads(f.read())
+
+    xs = [x for x, _ in map["map"]]
+    ys = [y for y, _ in map["map"]]
+    width = max(xs) - min(xs)
+    height = max(ys) - min(ys)
+
+    plt.title(f"Map: {map_path}")
+    fig = plt.figure()
+    skill_subfigures = fig.subfigures(1, num_skill_levels)
+
+    for i, skill_log_obj in enumerate(logs):
+        skill = skill_log_obj["skill"]
+        skill_logs = skill_log_obj["logs"]
+
+        subfig = skill_subfigures[i]
+        subfig.suptitle(f"skill: {skill}")
+        ax1, ax2 = subfig.subplots(2, 1, gridspec_kw={"height_ratios": [2, 1]})
+
+        ax1.set_aspect(width / height)
+        ax1.axis("off")
+        ax1.invert_yaxis()
+        ax1.set_facecolor("#bbddff")
+
+        ax1.fill(
+            *list(zip(*map["map"])), facecolor="#bbff66", edgecolor="black", linewidth=1
         )
-        last_x = end_x
-        last_y = end_y
+
+        start_x, start_y = map["start"]
+        ax1.plot(start_x, start_y, "bo")
+        target_x, target_y = map["target"]
+        ax1.plot(target_x, target_y, "ro")
+
+        if "sand traps" in map:
+            for trap in map["sand traps"]:
+                ax1.fill(
+                    *list(zip(*trap)),
+                    facecolor="#ffffcc",
+                    edgecolor="black",
+                    linewidth=1,
+                )
+
+        opacity = max(1 / len(logs), 0.05)
+        for log in skill_logs:
+            lands = log["landing_history"]["0"]
+            ends = log["ending_history"]["0"]
+
+            last_x = start_x
+            last_y = start_y
+            for n in range(len(ends)):
+                land_x, land_y = lands[n]
+                end_x, end_y = ends[n]
+                ax1.plot(
+                    [last_x, land_x], [last_y, land_y], color="black", alpha=opacity
+                )
+                ax1.arrow(
+                    land_x,
+                    land_y,
+                    end_x - land_x,
+                    end_y - land_y,
+                    color="black",
+                    alpha=0.5,
+                    head_width=4,
+                    head_length=4,
+                    length_includes_head=True,
+                )
+                last_x = end_x
+                last_y = end_y
+
+        shot_count = {}
+        for log in skill_logs:
+            score = log["scores"][0]
+            if not score in shot_count:
+                shot_count[score] = 0
+            shot_count[score] += 1
+
+        ax2.bar(*list(zip(*shot_count.items())))
+        ax2.set_xlabel("shots")
+        ax2.set_ylabel("trials")
+        ax2.set_xticks(list(shot_count.keys()))
+
+    map_basename = path.splitext(map_path)[0]
+    target_path = path.join("plots", f"{map_basename}.svg")
+    os.makedirs(path.dirname(target_path), exist_ok=True)
+    plt.savefig(target_path)
 
 
-shot_count = {}
-for log in logs:
-    score = log["scores"][0]
-    if not score in shot_count:
-        shot_count[score] = 0
-    shot_count[score] += 1
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python plot_trials.py <log_file.json>")
+        exit()
 
-ax2.bar(*list(zip(*shot_count.items())))
+    with open(sys.argv[1]) as f:
+        logs = json.loads(f.read())
 
-plt.savefig("render.svg")
+    plot_trials(logs)
---
+++ run_trials.py
@@ -0,0 +1,60 @@
+import subprocess
+import argparse
+import json
+from itertools import product
+from glob import glob
+from random import randint
+import multiprocessing
+import sys
+import os.path as path
+import os
+
+LOG_DIR = "trials"
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--skill", type=int, nargs="+", default=[10, 50, 100])
+parser.add_argument("--trials", type=int, default=5)
+parser.add_argument(
+    "--maps",
+    type=str,
+    default="maps/**/*.json",
+    help="glob pattern for matching map files",
+)
+args = parser.parse_args()
+
+
+def run_trial(pargs):
+    map, skill = pargs
+    trials = []
+    for trial in range(args.trials):
+        proc = subprocess.run(
+            f"python main.py --map {map} --skill {skill} --seed {randint(0, sys.maxsize)} --disable_logging --no_gui",
+            shell=True,
+            stdout=subprocess.PIPE,
+        )
+        trials.append(json.loads(proc.stdout))
+    print("Done:", map, skill)
+    return trials
+
+
+to_run = list(product(glob(args.maps), args.skill))
+print(to_run)
+
+# Run in parallel using all CPU cores
+pool = multiprocessing.Pool(multiprocessing.cpu_count())
+results = pool.map(run_trial, to_run)
+
+to_render = {}
+for result in results:
+    logs = result
+    map = logs[0]["map"]
+    skill = logs[0]["skills"][0]
+    if not map in to_render:
+        to_render[map] = []
+    to_render[map].append({"skill": skill, "logs": logs})
+
+for map in to_render:
+    target_path = path.join(LOG_DIR, map)
+    os.makedirs(path.dirname(target_path), exist_ok=True)
+    with open(target_path, "w") as f:
+        f.write(json.dumps(to_render[map]))
