fix: formula of distance.

players/g1_player.py
@@ -47,7 +47,7 @@ def splash_zone(distance: float, angle: float, conf: float, skill: int, current_
     if in_sandtrap:
         # the distance rating is halved, and the standard deviations for the angle and distance distributions are doubled
         distances = np.vectorize(standard_ppf)(
-            conf_points) * (distance / skill) * 2
+            conf_points) * (distance / skill) * 2 + distance / 2
         angles = np.vectorize(standard_ppf)(
             conf_points) * (1/(2*skill)) * 2 + angle
     else: